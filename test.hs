2959ffcc39b371d452c59aa353d15652a65fc694
sonesuke <iamsonesuke@gmail.com>
2013-04-20 22:00:00 +0900

    define operators

--- test.hs
+++ test.hs
@@ -5,6 +5,32 @@ import Text.Peggy
 import qualified Data.ByteString as B
 import qualified Data.ListLike as LL
 
+add :: [[Int]] -> [[Int]] -> [[Int]]
+add x y   |  length x > length y = left_melt x y
+           | otherwise           = right_melt x y 
+
+every :: [[Int]] -> [[Int]] -> [[Int]]
+every [] y = []
+every (x:xs) y = (map (\i -> x++i) y) ++ every xs y
+
+melt :: [[Int]] -> [[Int]] -> [[Int]]
+melt = left_melt
+
+left_melt :: [[Int]] -> [[Int]] -> [[Int]]
+left_melt x y = left_melt_internal x y y
+
+left_melt_internal :: [[Int]] -> [[Int]] -> [[Int]] -> [[Int]]
+left_melt_internal [] _ _ = []
+left_melt_internal x [] m  = left_melt_internal x m m 
+left_melt_internal (x:xs) (y:ys) m = [x ++ y] ++ left_melt_internal xs ys m
+
+right_melt :: [[Int]] -> [[Int]] -> [[Int]]
+right_melt x y = right_melt_internal x y x
+
+right_melt_internal :: [[Int]] -> [[Int]] -> [[Int]] -> [[Int]]
+right_melt_internal _ [] _ = []
+right_melt_internal [] y m  = right_melt_internal m y m 
+right_melt_internal (x:xs) (y:ys) m = [x ++ y] ++ right_melt_internal xs ys m
 
 data TestDef
   = TestFactor (String, [String])
